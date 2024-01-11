6d7730e0c1a6c2fd4aef918ba0d96163d680a623
Shashwat Singh <shashwat123student@gmail.com>
2024-01-11 21:36:53 +0530

    Imported and used glide for setting image

--- app/src/main/java/com/example/appoftheday/MainActivity.kt
+++ app/src/main/java/com/example/appoftheday/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.appoftheday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,8 +22,16 @@ class MainActivity : AppCompatActivity() {
         binding.buttonSetImage.setOnClickListener {
             binding.textTitle.text = "App of the Day"
 
-            val drawable = ContextCompat.getDrawable(this, R.drawable.test2)
-            binding.imageDownloaded.setImageDrawable(drawable)
+
+            Glide
+                .with(this)
+                .load("https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg")
+//            .centerCrop()
+//            .placeholder(R.drawable.loading_spinner)
+                .into(binding.imageDownloaded);
+
+//            val drawable = ContextCompat.getDrawable(this, R.drawable.test2)
+//            binding.imageDownloaded.setImageDrawable(drawable)
         }
     }
 }
