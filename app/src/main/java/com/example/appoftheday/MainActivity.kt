f814f93fb3782c27a5b10331df9310cd444c769f
Shashwat Singh <shashwat123student@gmail.com>
2024-05-30 01:10:10 +0530

    Learned about setting up the image

--- app/src/main/java/com/example/appoftheday/MainActivity.kt
+++ app/src/main/java/com/example/appoftheday/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.appoftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ContextMenu
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.appoftheday.databinding.ActivityMainBinding
@@ -16,5 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.buttonSetImage.setOnClickListener{
+            Log.d("Messsage","The Button has been clicked");
+            binding.imageDownloaded.setImageResource(R.drawable.test2);
+            val t1 = Toast.makeText(this,"The image has been set",Toast.LENGTH_LONG);
+            t1.show()
+        }
     }
 }
